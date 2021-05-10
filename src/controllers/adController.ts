03ad0afaeccfe618df320482e90a7926f85db865
Uyen Tran <utra0001@student.monash.edu>
2021-05-10 22:24:15 +1000

    Update swagger doc and add comment for AdController

--- src/controllers/adController.ts
+++ src/controllers/adController.ts
@@ -15,9 +15,34 @@ export class AdController {
       endDate,
     } = queryParams;
     const politicalInt = political?.map((e) => parseInt(e));
+
     // TODO: Testing needed to confirm different combinations of query params work
+    let findOptions: FindManyOptions = {
+      take: limit ? limit : 30,
+      skip: offset ? offset : 0,
+      select: ["id"],
+      join: {
+        alias: "ad",
+        leftJoin: {
+          adTags: "ad.adTags",
+        },
+        innerJoin: {
+          tags: "adTags.tag",
+          bot: "ad.bot",
+        },
+      },
+    };
 
     // Workaround for finding entity with relation condition: https://github.com/typeorm/typeorm/issues/4396#issuecomment-566254087
+
+    /**
+     * All WHERE conditions to filter ads in the database
+     *
+     * Notes:
+     * - Tight connescence with SQL, more specifically POSTGRES, syntax and functions
+     * - Tight connescence with the table alias used in findOptions
+     * - Each condition / element in whereConditions is joined together using an AND
+     */
     const whereConditions: any[][] = [];
     if (gender) {
       whereConditions.push(["bot.gender ILIKE ANY(:gender)", { gender }]);
@@ -46,22 +71,6 @@ export class AdController {
       whereConditions.push(["ad.createdAt <= :endDate", { endDate }]);
     }
 
-    let findOptions: FindManyOptions = {
-      take: limit ? limit : 30,
-      skip: offset ? offset : 0,
-      select: ["id"],
-      join: {
-        alias: "ad",
-        leftJoin: {
-          adTags: "ad.adTags",
-        },
-        innerJoin: {
-          tags: "adTags.tag",
-          bot: "ad.bot",
-        },
-      },
-    };
-
     if (whereConditions.length > 0) {
       findOptions.where = (qb: any) => {
         for (let i = 0; i < whereConditions.length; i++) {
@@ -73,8 +82,11 @@ export class AdController {
         }
       };
     }
+
+    // get ad ids that fit the options
     const adIds = (await Ad.find(findOptions)).map((e) => e.id);
 
+    // get ads with the required relations and data
     return await Ad.find({
       relations: ["bot", "adTags", "adTags.tag"],
       where: {
