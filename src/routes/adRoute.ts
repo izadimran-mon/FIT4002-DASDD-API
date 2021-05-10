9026dec9f1dd62cf3d30b7a0b0fd4221054e2c82
Uyen Tran <utra0001@student.monash.edu>
2021-05-10 19:53:01 +1000

    ad filter by start and end date

--- src/routes/adRoute.ts
+++ src/routes/adRoute.ts
@@ -19,8 +19,16 @@ router.get("/", async (req: Request, res: Response) => {
    *
    * # sort ads:
    */
-  let { offset, limit, political, gender, tag, bots } = req.query;
-
+  let {
+    offset,
+    limit,
+    political,
+    gender,
+    tag,
+    bots,
+    startDate,
+    endDate,
+  } = req.query;
   // TODO: Testing needed to confirm different combinations of query params work
   const queryParams = {
     offset: offset ? parseInt(offset as string) : 0,
@@ -30,6 +38,14 @@ router.get("/", async (req: Request, res: Response) => {
     gender: typeof gender === "string" ? [gender] : (gender as string[]),
     tag: typeof tag === "string" ? [tag] : (tag as string[]),
     bots: typeof bots === "string" ? [bots] : (bots as string[]),
+    startDate:
+      typeof startDate === "string"
+        ? new Date(parseInt(startDate as string))
+        : null,
+    endDate:
+      typeof endDate === "string"
+        ? new Date(parseInt(endDate as string))
+        : null,
   };
   if (queryParams.offset < 0 || queryParams.limit < 0) {
     res.send([]);
