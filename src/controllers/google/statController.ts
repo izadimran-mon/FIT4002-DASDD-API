6b59014bb32b7287e03edcab996cb0adcb2e1466
azha0018 <azha0018@student.monash.edu>
2021-08-18 21:17:32 +1000

    fix for timezone in stats ad count

--- src/controllers/google/statController.ts
+++ src/controllers/google/statController.ts
@@ -75,15 +75,17 @@ export class GoogleStatController {
   }
 
   async getAdCounts(startDate: Date) {
-    const start = `${startDate.getFullYear()}-${startDate.getMonth() + 1}-01`;
-    const endDate = new Date(startDate.setMonth(startDate.getMonth() + 1));
-    const end = `${endDate.getFullYear()}-${endDate.getMonth() + 1}-01`;
-
+    var start = new Date(
+      Date.UTC(startDate.getFullYear(), startDate.getMonth(), 1, 0, 0, 0)
+    );
+    var end = new Date(
+      Date.UTC(startDate.getFullYear(), startDate.getMonth() + 1, 0, 0, 0)
+    );
     const googleAdTableName = GoogleAd.getRepository().metadata.tableName;
     const rawRes = await getConnection().manager.query(
       `
         SELECT date, count(a."createdAt") AS count
-        FROM  generate_series($1::date, $2::date, interval '1 day') g(date)
+        FROM  generate_series($1::timestamp, $2::timestamp, interval '1 day') g(date)
         LEFT JOIN ${googleAdTableName} a ON a."createdAt" >= g.date
                         AND a."createdAt"  <  g.date + interval '1 day'
         GROUP  BY 1
