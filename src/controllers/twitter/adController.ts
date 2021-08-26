6cd099876462c5be9dc6a1a7186a2ae37299b59b
Uyen Tran <utra0001@student.monash.edu>
2021-08-26 21:40:23 +1000

    get ads grouped by id

--- src/controllers/twitter/adController.ts
+++ src/controllers/twitter/adController.ts
@@ -23,144 +23,7 @@ interface Links {
 }
 
 export class TwitterAdController {
-  async getAll(
-    queryParams: PaginationParams & TwitterAdFilterParams
-  ): Promise<{ metadata: Metadata; records: TwitterAd[] }> {
-    const {
-      limit,
-      offset,
-      // gender,
-      tag,
-      political,
-      bots,
-      startDate,
-      endDate,
-    } = queryParams;
-    const politicalInt = political?.map((e) => parseInt(e));
-
-    // TODO: Testing needed to confirm different combinations of query params work
-    let findOptions: FindManyOptions = {
-      take: limit ? limit : 30,
-      skip: offset ? offset : 0,
-      select: ["id", "createdAt"],
-      join: {
-        alias: "ad",
-        leftJoin: {
-          adTags: "ad.adTags",
-          tags: "adTags.tag",
-          bot: "ad.bot",
-        },
-      },
-      order: {
-        createdAt: "DESC",
-      },
-    };
-
-    // Workaround for finding entity with relation condition: https://github.com/typeorm/typeorm/issues/4396#issuecomment-566254087
-
-    /**
-     * All WHERE conditions to filter ads in the database
-     *
-     * Notes:
-     * - Tight connescence with SQL, more specifically POSTGRES, syntax and functions
-     * - Tight connescence with the table alias used in findOptions
-     * - Each condition / element in whereConditions is joined together using an AND
-     */
-    const whereConditions: any[][] = [];
-    // if (gender) {
-    //   whereConditions.push(["bot.gender ILIKE ANY(:gender)", { gender }]);
-    // }
-
-    if (tag) {
-      whereConditions.push(["tags.name ILIKE ANY(:tag)", { tag }]);
-    }
-
-    if (political) {
-      whereConditions.push([
-        "bot.politicalRanking = ANY(:political)",
-        { political: politicalInt },
-      ]);
-    }
-
-    if (bots) {
-      whereConditions.push(["bot.id = ANY(:bots)", { bots }]);
-    }
-
-    // if (startDate) {
-    //   whereConditions.push(["ad.createdAt >= :startDate", { startDate }]);
-    // }
-
-    // if (endDate) {
-    //   whereConditions.push(["ad.createdAt <= :endDate", { endDate }]);
-    // }
-
-    if (whereConditions.length > 0) {
-      findOptions.where = (qb: any) => {
-        for (let i = 0; i < whereConditions.length; i++) {
-          if (i == 0) {
-            qb.where(whereConditions[i][0], whereConditions[i][1]);
-          } else {
-            qb.andWhere(whereConditions[i][0], whereConditions[i][1]);
-          }
-        }
-      };
-    }
-
-    // get ad ids that fit the options
-    const adIds = (await TwitterAd.find(findOptions)).map((e) => e.id);
-
-    const filteredAdNumber = adIds.length;
-
-    const ads = await TwitterAd.find({
-      relations: ["bot", "adTags", "adTags.tag"],
-      where: {
-        id: In(adIds),
-      },
-    });
-
-    let currentOffset = 0;
-    let currentLimit = 30;
-
-    if (offset !== undefined) {
-      currentOffset = offset;
-    }
-
-    if (limit !== undefined) {
-      currentLimit = limit;
-    }
-
-    const currentPage = currentOffset / currentLimit;
-
-    delete findOptions.take;
-    delete findOptions.skip;
-
-    const totalAdNumber = await TwitterAd.count(findOptions);
-
-    let currentLink: Links = {
-      self: "",
-      first: "",
-      previous: "",
-      next: "",
-      last: "",
-    };
-
-    // get meta data
-    const metadataForAd: Metadata = {
-      page: currentPage,
-      per_page: currentLimit,
-      page_count: filteredAdNumber,
-      total_count: totalAdNumber,
-      links: currentLink,
-    };
-
-    // get ads with the required relations and data
-    return {
-      metadata: metadataForAd,
-      records: ads,
-    };
-  }
-
-  async getAllNew(
+  async getAdInstances(
     queryParams: PaginationParams & TwitterAdFilterParams
   ): Promise<{ metadata: Metadata; records: any[] }> {
     const {
@@ -216,13 +79,13 @@ export class TwitterAdController {
       whereConditions.push(["bot.id = ANY(:bots)", { bots }]);
     }
 
-    // if (startDate) {
-    //   whereConditions.push(["ad.createdAt >= :startDate", { startDate }]);
-    // }
+    if (startDate) {
+      whereConditions.push(["adBot.createdAt >= :startDate", { startDate }]);
+    }
 
-    // if (endDate) {
-    //   whereConditions.push(["ad.createdAt <= :endDate", { endDate }]);
-    // }
+    if (endDate) {
+      whereConditions.push(["adBot.createdAt <= :endDate", { endDate }]);
+    }
 
     if (whereConditions.length > 0) {
       findOptions.where = (qb: any) => {
@@ -266,6 +129,142 @@ export class TwitterAdController {
     delete findOptions.skip;
 
     const totalAdNumber = await TwitterAdSeenByBot.count(findOptions);
+
+    let currentLink: Links = {
+      self: "",
+      first: "",
+      previous: "",
+      next: "",
+      last: "",
+    };
+
+    // get meta data
+    const metadataForAd: Metadata = {
+      page: currentPage,
+      per_page: currentLimit,
+      page_count: filteredAdNumber,
+      total_count: totalAdNumber,
+      links: currentLink,
+    };
+
+    // get ads with the required relations and data
+    return {
+      metadata: metadataForAd,
+      records: ads,
+    };
+  }
+
+  async getAdUniques(
+    queryParams: PaginationParams & TwitterAdFilterParams
+  ): Promise<{ metadata: Metadata; records: any[] }> {
+    const {
+      limit,
+      offset,
+      // gender,
+      tag,
+      political,
+      bots,
+      startDate,
+      endDate,
+    } = queryParams;
+    const politicalInt = political?.map((e) => parseInt(e));
+
+    // TODO: Testing needed to confirm different combinations of query params work
+    let findOptions: FindManyOptions = {
+      take: limit ? limit : 30,
+      skip: offset ? offset : 0,
+      select: ["id"],
+      join: {
+        alias: "ad",
+        leftJoin: {
+          adBot: "ad.adBot",
+          bot: "adBot.bot",
+          adTags: "ad.adTags",
+          tags: "adTags.tag",
+        },
+      },
+      order: {
+        id: "DESC",
+      },
+    };
+
+    // Workaround for finding entity with relation condition: https://github.com/typeorm/typeorm/issues/4396#issuecomment-566254087
+
+    const whereConditions: any[][] = [];
+    // if (gender) {
+    //   whereConditions.push(["bot.gender ILIKE ANY(:gender)", { gender }]);
+    // }
+
+    if (tag) {
+      whereConditions.push(["tags.name ILIKE ANY(:tag)", { tag }]);
+    }
+
+    if (political) {
+      whereConditions.push([
+        "bot.politicalRanking = ANY(:political)",
+        { political: politicalInt },
+      ]);
+    }
+
+    if (bots) {
+      whereConditions.push(["bot.id = ANY(:bots)", { bots }]);
+    }
+
+    // if (startDate) {
+    //   whereConditions.push(["adBot.createdAt >= :startDate", { startDate }]);
+    // }
+
+    // if (endDate) {
+    //   whereConditions.push(["adBot.createdAt <= :endDate", { endDate }]);
+    // }
+
+    if (whereConditions.length > 0) {
+      findOptions.where = (qb: any) => {
+        for (let i = 0; i < whereConditions.length; i++) {
+          if (i == 0) {
+            qb.where(whereConditions[i][0], whereConditions[i][1]);
+          } else {
+            qb.andWhere(whereConditions[i][0], whereConditions[i][1]);
+          }
+        }
+      };
+    }
+
+    // get ad ids that fit the options
+    const adIds = (await TwitterAd.find(findOptions)).map(
+      (e: TwitterAd) => e.id
+    );
+
+    const filteredAdNumber = adIds.length;
+
+    const ads: any[] = await TwitterAd.find({
+      relations: ["adBot", "adBot.bot", "adTags", "adTags.tag"],
+      where: {
+        id: In(adIds),
+      },
+    });
+    ads.map((e): any => {
+      e.seenInstances = e.adBot;
+      delete e.adBot;
+    });
+
+    let currentOffset = 0;
+    let currentLimit = 30;
+
+    if (offset !== undefined) {
+      currentOffset = offset;
+    }
+
+    if (limit !== undefined) {
+      currentLimit = limit;
+    }
+
+    const currentPage = currentOffset / currentLimit;
+
+    delete findOptions.take;
+    delete findOptions.skip;
+
+    const totalAdNumber = await TwitterAd.count(findOptions);
 
     let currentLink: Links = {
       self: "",
