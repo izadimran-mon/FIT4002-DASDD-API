6bac35a43a2420c530c1dbb2a062e9b068795699
Xi Zhang <xixixixizhang28@gmail.com>
2021-09-01 18:08:29 +1000

    Create the own data

--- src/tests/__tests__/routes/googleAd.test.ts
+++ src/tests/__tests__/routes/googleAd.test.ts
@@ -390,7 +390,65 @@ describe("GET /google/ads", () => {
 
 describe("GET /google/ads/:id", () => {
   beforeEach(async (done) => {
-    await connection.createTestData();
+    // Create test data
+    const bot1 = GoogleBot.create({
+      id: "919222a3-c13e-4c8e-8f23-82fa872512cf",
+      username: "bot1",
+      dob: new Date("1999-07-14"),
+      gender: "male",
+      fName: "First",
+      lName: "Bot",
+      otherTermsCategory: 0,
+      password: "password123",
+      locLat: -23.139826,
+      locLong: 34.139062,
+      type: "google",
+      politicalRanking: 0,
+    });
+
+    await GoogleBot.save([bot1]);
+
+    const tag1 = GoogleTag.create({ name: "Tech" });
+    const tag2 = GoogleTag.create({ name: "Food" });
+    const tag3 = GoogleTag.create({ name: "Education" });
+
+    await GoogleTag.save([tag1, tag2, tag3]);
+
+    const ad1 = GoogleAd.create({
+      id: "3883387e-8431-4cf6-ad87-6b274a882ff9",
+      bot: bot1,
+      createdAt: new Date("2020-11-20 23:52:56"),
+      image: "https://project.s3.region.amazonaws.com/image_3.png",
+      seenOn: "https://www.bbc.com/news/science-environment-54395534",
+      loggedIn: false,
+      headline: "Headline 1",
+      html: "innerHTML",
+      adLink: "www.donuts.com/",
+    });
+
+    const ad2 = GoogleAd.create({
+      id: "3883387e-8431-4cf6-ad87-6b274a882ff1",
+      bot: bot1,
+      createdAt: new Date("2020-11-11 23:52:56"),
+      image: "https://project.s3.region.amazonaws.com/image_3.png",
+      seenOn: "https://www.bbc.com/news/science-environment-54395534",
+      loggedIn: false,
+      headline: "Headline 1",
+      html: "innerHTML",
+      adLink: "www.donuts.com/",
+    });
+
+    await GoogleAd.save([ad1, ad2]);
+
+    const adTagsData: DeepPartial<GoogleAdTag>[] = [
+      {
+        ad: ad2,
+        tag: tag1,
+      },
+    ];
+    const adTags = adTagsData.map((a) => GoogleAdTag.create(a));
+    await GoogleAdTag.save(adTags);
+
     done();
   });
 
